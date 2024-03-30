c49cde43288ab01c688d62189e27d9e4c0a1aade
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-03-29 20:16:52 -0700

    v 20240329
    added some lines to credentials file

--- install-workstation.sh
+++ install-workstation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-# Version 20240317
+# Version 20240329
 #
 ####################
 # SCRIPT PARAMETERS
@@ -14,15 +14,16 @@ RPM_URL='https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/8/c
 ##########################
 # DETERMINE LINUX VARIENT
 ##########################
-if test -f /etc/lsb-release; then . /etc/lsb-release; fi
-if [ "x$DISTRIB_ID" = "xUbuntu" ] || [ "x$DISTRIB_ID" = "xLinuxMint" ]; then URL="$DEB_URL"; else URL="$RPM_URL"; fi
+if test -f /etc/os-release; then . /etc/os-release; fi
+if [ "x$ID" = "xubuntu" ] || [ "x$ID" = "xlinuxmint" ] || [ "x$ID" = "xdebian" ]; then URL="$DEB_URL"; else URL="$RPM_URL"; fi
+if [ "x$ID" = "x" ]; then echo "Cannot determine OS variant. Terminating script"; exit; fi
 PKG=`echo $URL | cut -d "/" -f 10`
 
 ########################################
 # DOWNLOAD AND INSTALL CHEF WORKSTATION
 ########################################
 wget -O "$PKG" "$URL"
-if test "x$DISTRIB_ID" = "xUbuntu"
+if [ "x$ID" = "xubuntu" ] || [ "x$ID" = "xlinuxmint" ] || [ "x$ID" = "xdebian" ]
   then sudo dpkg -i "$PKG"
   else sudo yum localinstall "$PKG"
 fi
@@ -34,8 +35,10 @@ if ! test -d ~/.chef; then mkdir ~/.chef; fi
 echo "[default]
 client_name     = '$CHEF_ADMIN_ID'
 client_key      = '/home/$CHEF_ADMIN_ID/.chef/$CHEF_ADMIN_ID.pem'
+validation_client_name = '$CHEF_ORG'
+validation_client_key = '/home/$CHEF_ADMIN_ID/.chef/$CHEF_ORG-validator.pem'
 chef_server_url = 'https://$CHEF_SERVER_NAME/organizations/$CHEF_ORG'
-cookbook_path  =  'home/$CHEF_ADMIN_ID/$CHEF_REPO'
+cookbook_path  =  'home/$CHEF_ADMIN_ID/$CHEF_REPO/cookbooks'
 " > ~/.chef/credentials
 
 #############
@@ -48,7 +51,7 @@ echo 'eval "$(chef shell-init bash)"' | tee -a ~/.bashrc >> /dev/null
 ############################
 # GENERATE DEFAULT CHEF REPO
 ############################
-chef generate repo "$CHEF_REPO" --chef-license 'accept'
+if ! test -d "$CHEF_REPO"; then chef generate repo "$CHEF_REPO" --chef-license 'accept'; fi
 
 echo "###########################"
 echo "# CHEF WORKSTATION REBOOT #"
@@ -57,6 +60,5 @@ echo '###########################'
 echo "Script will automatically reboot server now"
 read -p "Press Enter to reboot, CTRL-C to abort reboot" REBOOT
 shutdown -r now
-
 
 
