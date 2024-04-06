70ef4270284b74ddfc25facff7f7688dfb3dfb2c
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-04-06 13:50:14 -0700

    Updated 20240406

--- prep-workstation.sh
+++ prep-workstation.sh
@@ -30,7 +30,7 @@ sudo apt install software-properties-common -y
 sudo apt install apt-transport-https -y
 wget -O 'code.deb' 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 sudo dpkg -i code.deb; rm code.deb
-sudo apt install vagrant -y
+#sudo apt install vagrant -y
 #####################################
 # END LOAD APPLICATIONS SECTION
 #####################################
