3478b6b463a433eada2a9f1ae8d020411ecf7d39
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-04-06 13:48:42 -0700

    Updated 20240406

--- prep-workstation.sh
+++ prep-workstation.sh
@@ -1,7 +1,19 @@
-# version='20240329'
+# version='20240406'
 #
-CHEF_GIT_USER='mbomba'
+CHEF_GIT_USER='mdbomba'
 CHEF_GIT_EMAIL='mbomba@kemptechnologies.com'
+
+###################
+# PARAMETER CHECK
+##################
+echo ''
+echo 'SCRIPT TO INSTALL PACKAGES CHEF WORKSTATION DEPENDS ON'
+echo 'SCRIPT IS INTENDED TO BE EDITED AND PROPER PARAMETERS ENTERED BEFORE RUNNING'
+echo 'PARAMETERS CURRENTLY CONFIGURED IN SCRIPT'
+echo "    CHEF_GIT_USER = $CHEF_GIT_USER"
+echo "    CHEF_GIT_EMAIL = $CHEF_GIT_EMAIL"
+read -p "Press any key to continue, CTRL-C to abort and edit values: " YN
+
 #####################################
 # START LOAD APPLICATIONS SECTION
 #####################################
