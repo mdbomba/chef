856f43ce2a7f74f1c524cc14803df1583e5d79dd
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-04-06 12:48:33 -0700

    update 20240406
    Added check values prompt

--- install-server.sh
+++ install-server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash +x
 # Version 20240317
 #
 ##################
@@ -8,10 +8,26 @@ CHEF_ADMIN_ID='chef'
 CHEF_ADMIN_FIRST='Chef'
 CHEF_ADMIN_LAST='Admin'
 CHEF_ADMIN_EMAIL='chef.admin@kemptech.biz'
-read -p  "Enter password for Chef Admin Account ($CHEF_ADMIN_ID): " CHEF_ADMIN_PASS
+CHEF_ADMIN_PASS='devsecops'
 CHEF_ORG='progress'
 CHEF_ORG_LONG="Progress Software"
 
+#######################
+# ASK IF VALUES ARE OK
+#######################
+echo ''
+echo 'THIS SCRIPT WILL INSTALL CHEF AUTOMATE WITH CHEF INFRA SERVER'
+echo 'THIS SCRIPT IS INTENDED TO BE EDITED TO SET PARAMETERS BEFORE BEING RUN'
+echo 'PARAMETER LIST FOR THIS SCRIPT IS LISTED BELOW'
+echo "    CHEF_ADMIN_ID = $CHEF_ADMIN_ID"
+echo "    CHEF_ADMIN_FIRST = $CHEF_ADMIN_FIRST"
+echo "    CHEF_ADMIN_LAST = $CHEF_ADMIN_LAST"
+echo "    CHEF_ADMIN_EMAIL = $CHEF_ADMIN_EMAIL"
+echo "    CHEF_ADMIN_PASS = $CHEF_ADMIN_PASS"
+echo "    CHEF_ORG = $CHEF_ORG"
+echo "    CHEF_ORG_LONG = $CHEF_ORG_LONG"
+read -p "Press any key to continue, CTRL-C to terminate and edit values" YN
+	
 ########################
 # UPDATE sysctl.conf
 ########################
@@ -29,9 +45,13 @@ sudo  sysctl -w vm.dirty_expire_centisecs=20000
 #######################################################################
 # DOWNLOAD AND INSTALL CHEF SERVER (AUTOMATE + INFRA_SERVER + BUILDER)
 #######################################################################
-curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
-sudo ./chef-automate deploy --product automate --product infra-server
-sudo chef-automate init-config
+if [ `command -v chef-server-ctl` ]; then
+  echo 'Chef Automate already installed, Proceeding to create user/org'
+else
+  curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
+  sudo ./chef-automate deploy --product automate --product infra-server
+  sudo chef-automate init-config
+fi
 sudo chef-server-ctl user-create "$CHEF_ADMIN_ID" "$CHEF_ADMIN_FIRST" "$CHEF_ADMIN_LAST" "$CHEF_ADMIN_EMAIL" "$CHEF_ADMIN_PASS" --filename "$CHEF_ADMIN_ID"".pem"
 sudo chef-server-ctl org-create "$CHEF_ORG" "$CHEF_ORG_LONG" --association_user "$CHEF_ADMIN_ID" --filename "$CHEF_ORG""-validator.pem"
 
