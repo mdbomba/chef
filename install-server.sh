2306535cebcea7c9004e02e33e8e52148d649a0a
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-11-18 19:12:03 -0700

    20241118

--- install-server.sh
+++ install-server.sh
@@ -4,13 +4,13 @@
 #############
 # PARAMETERS
 #############
-CHEF_ADMIN_ID='chef'
-CHEF_ADMIN_FIRST='Chef'
-CHEF_ADMIN_LAST='Admin'
-CHEF_ADMIN_EMAIL='chef.admin@kemptech.biz'
-CHEF_ADMIN_PASS='devsecops'
-CHEF_ORG='progress'
-CHEF_ORG_LONG="Progress Software"
+ADMIN='chef'
+ADMIN_FIRST='Chef'
+ADMIN_LAST='Chef'
+ADMIN_EMAIL='chef@kemptech.biz'
+ADMIN_PASS='devsecops'
+ORG='demoLab'
+ORG_LONG="Progress Software Demo Lab"
 
 #######################
 # ASK IF VALUES ARE OK
@@ -19,28 +19,36 @@ echo ''
 echo 'THIS SCRIPT WILL INSTALL CHEF AUTOMATE WITH CHEF INFRA SERVER'
 echo 'THIS SCRIPT IS INTENDED TO BE EDITED TO SET PARAMETERS BEFORE BEING RUN'
 echo 'PARAMETER LIST FOR THIS SCRIPT IS LISTED BELOW'
-echo "    CHEF_ADMIN_ID = $CHEF_ADMIN_ID"
-echo "    CHEF_ADMIN_FIRST = $CHEF_ADMIN_FIRST"
-echo "    CHEF_ADMIN_LAST = $CHEF_ADMIN_LAST"
-echo "    CHEF_ADMIN_EMAIL = $CHEF_ADMIN_EMAIL"
-echo "    CHEF_ADMIN_PASS = $CHEF_ADMIN_PASS"
-echo "    CHEF_ORG = $CHEF_ORG"
-echo "    CHEF_ORG_LONG = $CHEF_ORG_LONG"
+echo "    ADMIN = $ADMIN"
+echo "    ADMIN_FIRST = $ADMIN_FIRST"
+echo "    ADMIN_LAST = $ADMIN_LAST"
+echo "    ADMIN_EMAIL = $ADMIN_EMAIL"
+echo "    ADMIN_PASS = $ADMIN_PASS"
+echo "    ORG_ = $ORG_"
+echo "    ORG__LONG = $ORG__LONG"
 read -p "Press any key to continue, CTRL-C to terminate and edit values" YN
 	
 ########################
 # UPDATE sysctl.conf
 ########################
-if ! test -f /etc/sysctl.conf_orig; then sudo cp -f /etc/sysctl.conf /etc/sysctl.conf_orig; fi
-if test -f /etc/sysctl.conf_orig; then sudo cp -f /etc/sysctl.conf_orig /etc/sysctl.conf; fi
-echo "vm.max_map_count=262144"         | sudo tee -a /etc/sysctl.conf
-echo "vm.dirty_expire_centisecs=20000" | sudo tee -a /etc/sysctl.conf
+sudo grep -v "vm.max_map_count" /etc/sysctl.conf           | sudo tee ./temp1.tmp >/dev/null
+sudo grep -v "vm.dirty_expire_centisecs=" /etc/sysctl.conf | sudo tee -a ./temp1.tmp >/dev/null
+echo "vm.max_map_count=262144"         | sudo tee -a ./temp1.tmp
+echo "vm.dirty_expire_centisecs=20000" | sudo tee -a ./temp1.tmp
+sudo cp -f ./temp1.tmp /etc/sysctl.conf 
 
 ########################
 # UPDATE RUNNING CONFIG
 ########################
 sudo  sysctl -w vm.max_map_count=262144
 sudo  sysctl -w vm.dirty_expire_centisecs=20000
+
+##########################
+# CHECK FOR PREREQUISITES
+##########################
+sudo apt install -y curl >/dev/null 2>&1 ; fi
+sudo apt install -y gzip >/dev/null 2>&1 ; fi
+sudo apt install -y openssh-server >/dev/null 2>&1 ; fi
 
 #######################################################################
 # DOWNLOAD AND INSTALL CHEF SERVER (AUTOMATE + INFRA_SERVER + BUILDER)
@@ -52,7 +60,11 @@ else
   sudo ./chef-automate deploy --product automate --product infra-server
   sudo chef-automate init-config
 fi
-sudo chef-server-ctl user-create "$CHEF_ADMIN_ID" "$CHEF_ADMIN_FIRST" "$CHEF_ADMIN_LAST" "$CHEF_ADMIN_EMAIL" "$CHEF_ADMIN_PASS" --filename "$CHEF_ADMIN_ID"".pem"
-sudo chef-server-ctl org-create "$CHEF_ORG" "$CHEF_ORG_LONG" --association_user "$CHEF_ADMIN_ID" --filename "$CHEF_ORG""-validator.pem"
+
+###############################################
+# CREATE ACCOUNTS FOR USER ADMIN AND ORG ADMIN
+###############################################
+sudo chef-server-ctl user-create "$ADMIN" "$ADMIN_FIRST" "$ADMIN_LAST" "$ADMIN_EMAIL" "$ADMIN_PASS" --filename "$ADMIN"".pem"
+sudo chef-server-ctl org-create "$ORG" "$ORG_LONG" --association_user "$ADMIN" --filename "$ORG""-validator.pem"
 
 
